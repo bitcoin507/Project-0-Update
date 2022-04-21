ae41153c3fb80a30ef5b0bc51e95713e0bd3dd92
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-04-21 20:34:10 +0100

    Linked script.js, added bacis method to pull movies

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,8 +1,27 @@
 
 var requesUrl = "https://api.themoviedb.org/" //"http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+var movieName = "Titanic";
 
-function getMoviebyName(movieName){
+
+getMoviebyName();
+
+function getMoviebyName() {
+    var movieGetUrl = "https://api.themoviedb.org/3/search/movie?query=" + movieName + "&api_key=5282afd0a67826fac3febca5930766eb";
+    fetch(movieGetUrl, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
 
 
+        }
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+
+        })
 
 }
