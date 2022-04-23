c9dae5f9ba9cbd9b63e869f25ae402370626a874
Don Avworo <101209035+DonAvworo@users.noreply.github.com>
2022-04-23 14:58:41 +0100

    commit

--- assets/scripts/translate.js
+++ assets/scripts/translate.js
@@ -1,23 +1,27 @@
-//target html main language to translate from and language to translate to
-const language1 = document.getElementById("#fromLanguage");
-const language2 = document.getElementById("#toLanguage");
 
 
-//function to fetch api
 function getLanguages() {
-    var myHeaders = new Headers();
-myHeaders.append("X-RapidAPI-Key", "3a9b9beb0amsh0cbdcdada685233p1fdeebjsn7477d1608833");
-
-var requestOptions = {
-  method: 'GET',
-  headers: myHeaders,
-  redirect: 'follow'
-};
-
-fetch("https://google-translate1.p.rapidapi.com/language/translate/v2/languages", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
+  var myHeaders = new Headers();
+  myHeaders.append("X-RapidAPI-Key", "3a9b9beb0amsh0cbdcdada685233p1fdeebjsn7477d1608833");
+  
+  var requestOptions = {
+      method: 'GET',
+      headers: myHeaders,
+      redirect: 'follow'
+  };
+  fetch("https://google-translate1.p.rapidapi.com/language/translate/v2/languages", requestOptions)
+      .then(response => response.json())
+      .then(result => createLanguagesList(result.data))
+      .catch(error => console.log('error', error));
 }
-
-//function to extract text from website 
+function createLanguagesList(langauges) {
+  var toLanguage = document.getElementById("toLanguage");
+  for (var i = 0; i < langauges.languages.length; i++) {
+      var language = langauges.languages[i]
+      var option = document.createElement("option");
+      option.value = language.language
+      option.text = language.language
+      toLanguage.add(option);
+  }
+}
+getLanguages();
