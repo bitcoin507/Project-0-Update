938c72c62394dc3dd6e6a944153016a6ad6ccccc
Chris Aylen <22746292+ChrisAylen@users.noreply.github.com>
2022-04-30 15:11:36 +0100

    Now pulls poster info

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,11 +9,13 @@ var movies = [];
 var multipage=false;
 var movieTitle="";
 var movieOverview="";
+var posterUrl="https://image.tmdb.org/t/p/w500";
 
 //var searchText = document.querySelector("#movieText");
 var input = document.getElementById("movieText");
 // var searchButton = document.querySelector("#search-btn");
 var movieToTranslateText = document.querySelector("#movieDetails");
+var moviePosterContainer=document.getElementById("moviePosterContainer")
 
 
 async function getMovieByName(movieName, pageNumber) {
@@ -44,6 +46,7 @@ async function getMovieByName(movieName, pageNumber) {
                 id: data.results[i].id,
                 title: data.results[i].title,
                 overview: data.results[i].overview,
+                poster_path: posterUrl + data.results[i].poster_path,
             }
             movies.push(movie);
         }
@@ -67,6 +70,10 @@ const onSearchMovie = () => {
 
         console.log(movies);
         movieToTranslateText.innerHTML = movies[0].title + ": " + movies[0].overview;
+        moviePosterContainer.style.backgroundImage = "url(" + movies[0].poster_path + ")";
+        moviePosterContainer.style.width = "500px";
+        moviePosterContainer.style.height = "800px";
+        //moviePosterContainer.style.backgroundImage = url(movies[0].poster_path);   //.posterUrl=movies[0].poster_path;
     })
 }
 
